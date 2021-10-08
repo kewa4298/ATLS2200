5fa0606e8c64087749f849be46b41cec2f1a10e1
kawaxman <kawaxman@icloud.com>
2021-10-08 16:31:51 -0600

    Week 6 Work Done

---
+++ week6/listeners.js
@@ -0,0 +1,7 @@
+document.getElementById("button").addEventListener("click", function() {
+    document.getElementById("button").innerHTML = "Button Clicked!";
+});
+
+document.getElementById("button1").addEventListener("click", function() {
+    document.getElementById("title").innerHTML = "Travis Scott";
+});
